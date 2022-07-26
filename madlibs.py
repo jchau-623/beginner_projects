ed9557526ff14fc6a67cc6ea7cd74e9be602f75f
justin chau <jchau623@gmail.com>
2022-07-26 12:35:10 -0400

    rock paper scissors

--- madlibs.py
+++ madlibs.py
@@ -9,7 +9,7 @@ youtuber = "Justin Chau" #some string variable
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
-celebrity = input("Famous Person: ")
+celebrity = input("Celebrity: ")
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. Stay hydrated and {verb2} like you are {celebrity}!"
--- random_number.py
+++ random_number.py
@@ -11,4 +11,22 @@ def guess(x):
             print("Too high! Guess again")
     print(f'You guessed the correct number! {random_number} is the correct number')
 
-guess(10)
+# guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high because low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'Computer has guessed {guess}, correctly!')
+
+computer_guess(1000)
